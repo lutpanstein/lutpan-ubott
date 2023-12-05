aae60ea1a15670cd151c61a8e6c3097467cafc51
Lutpan <145089991+lutpanstein@users.noreply.github.com>
2023-12-06 06:42:37 +0700

    Add files via upload

---
+++ userbot/clients/__init__.py
@@ -0,0 +1,11 @@
+# Man - UserBot
+# Copyright (c) 2022 Man-Userbot
+# Credits: @mrismanaziz || https://github.com/mrismanaziz
+#
+# This file is a part of < https://github.com/mrismanaziz/Man-Userbot/ >
+# t.me/SharingUserbot & t.me/Lunatic0de
+# remake by Lutpanstein t.me/Lutpanstein
+
+from .client_list import client_id, clients_list
+from .logger import man_userbot_on
+from .startup import man_client, multiman
